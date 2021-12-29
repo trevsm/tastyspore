42bbf4f002d425ec2eeb2290b5df5290d721f78f
Trevor <trevorsm@pm.me>
2021-12-28 22:04:32 -0700

    added default htaccess

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     "gatsby-plugin-breakpoints",
+    "gatsby-plugin-htaccess",
     {
       resolve: "gatsby-plugin-robots-txt",
       options: {
