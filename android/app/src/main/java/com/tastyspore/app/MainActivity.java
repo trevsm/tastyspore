f7c794ae9f1ef540c29ed100d06cd4cbe71c6585
Trevor <trevorsm@pm.me>
2022-01-07 15:56:17 -0700

    Added android app

---
+++ android/app/src/main/java/com/tastyspore/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tastyspore.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- docs/component---src-pages-404-tsx-781ca11f7ddfdf5d4d2e.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktastyspore=self.webpackChunktastyspore||[]).push([[218],{3205:function(e,t,n){n.r(t);var s=n(7294);t.default=function(){return s.createElement("div",null,"404")}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-781ca11f7ddfdf5d4d2e.js.map
